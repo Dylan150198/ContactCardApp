7184a80ad6a8b4b99a88cedecd6dbe566a4fc86e
Dylan ten Böhmer <djhbohme@avans.nl>
2018-11-15 14:40:48 +0100

    JSON ophalen gaat nu via een AsyncTask

--- app/src/main/java/com/example/dylan/contactcardapp/MainActivity.java
+++ app/src/main/java/com/example/dylan/contactcardapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.dylan.contactcardapp;
 
-import android.app.ProgressDialog;
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -18,6 +20,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
     private static final String URL_DATA = "https://randomuser.me/api/?results=50";
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
-    private List<Person> people;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,21 +36,82 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        people = new ArrayList<>();
         getData();
     }
-
     private void getData() {
+        getDataOperation getDataOperation = new getDataOperation(this);
+        getDataOperation.execute();
+//        final ProgressDialog progressDialog = new ProgressDialog(this);
+//        progressDialog.setMessage("Loading people...");
+//        progressDialog.show();
+//
+//        StringRequest stringRequest = new StringRequest(Request.Method.GET,
+//                URL_DATA, new Response.Listener<String>() {
+//            @Override
+//            public void onResponse(String response) {
+//                progressDialog.dismiss();
+//                try {
+//                    JSONObject jsonObject = new JSONObject(response);
+//                    JSONArray array = jsonObject.getJSONArray("results");
+//                    for (int i = 0; i < array.length(); i++){
+//                        JSONObject jo = array.getJSONObject(i);
+//                        String title = jo.getJSONObject("name").getString("title");
+//                        String first = jo.getJSONObject("name").getString("first");
+//                        String last = jo.getJSONObject("name").getString("last");
+//                        String thumbNail = jo.getJSONObject("picture").getString("thumbnail");
+//                        String medium = jo.getJSONObject("picture").getString("medium");
+//                        Person person = new Person(title, first, last, thumbNail, medium);
+//                        people.add(person);
+//                    }
+//                    adapter = new PersonAdapter(people, getApplicationContext());
+//                    recyclerView.setAdapter(adapter);
+//                } catch (JSONException e) {
+//
+//                    e.printStackTrace();
+//                }
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Toast.makeText(MainActivity.this, "Error" + error.toString(), Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
+//        RequestQueue requestQueue = Volley.newRequestQueue(this);
+//        requestQueue.add(stringRequest);
+    }
+    private class getDataOperation extends AsyncTask<Void, Void, Void> {
+        private List<Person> asyncPersonList;
+        private final WeakReference<Activity> mainActivity;
 
-        final ProgressDialog progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Loading people...");
-        progressDialog.show();
+        public getDataOperation(Activity mainActivity) {
+            this.mainActivity = new WeakReference<>(mainActivity);
+            this.asyncPersonList = new ArrayList<>();
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(getApplicationContext(), "The background thread is going to retrieve data separate from the UI thread", Toast.LENGTH_LONG).show();
+        }
 
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Toast.makeText(getApplicationContext(), "The background thread has retrieved data separate from the UI thread.", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+            Toast.makeText(getApplicationContext(), "The background is retrieving data separate from the UI thread.", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... people) {
         StringRequest stringRequest = new StringRequest(Request.Method.GET,
                 URL_DATA, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                progressDialog.dismiss();
                 try {
                     JSONObject jsonObject = new JSONObject(response);
                     JSONArray array = jsonObject.getJSONArray("results");
@@ -60,15 +123,16 @@ public class MainActivity extends AppCompatActivity {
                         String thumbNail = jo.getJSONObject("picture").getString("thumbnail");
                         String medium = jo.getJSONObject("picture").getString("medium");
                         Person person = new Person(title, first, last, thumbNail, medium);
-                        people.add(person);
+                        asyncPersonList.add(person);
                     }
-                    adapter = new PersonAdapter(people, getApplicationContext());
+                    adapter = new PersonAdapter(asyncPersonList, getApplicationContext());
                     recyclerView.setAdapter(adapter);
                 } catch (JSONException e) {
 
                     e.printStackTrace();
                 }
             }
+
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
@@ -76,7 +140,10 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        RequestQueue requestQueue = Volley.newRequestQueue(mainActivity.get());
         requestQueue.add(stringRequest);
+            return null;
+        }
     }
 }
+
